a35c0940f15f7e835f95cd937fa3fa52a6c4f3e1
juan_sca <juansca1229@gmail.com>
2017-04-12 11:53:27 -0300

    train.py modified. Now, we can select what model we want to use

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -1,29 +1,37 @@
 """Train an n-gram model.
 
 Usage:
-  train.py -n <n> -o <file>
+  train.py -n <n> [-m <model>] -o <file>
   train.py -h | --help
 
 Options:
   -n <n>        Order of the model.
+  -m <model>    Model to use [default: ngram]:
+                  ngram: Unsmoothed n-grams.
+                  addone: N-grams with add-one smoothing.
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
 from docopt import docopt
 import pickle
 
-from languagemodeling.ngram import NGram
+from languagemodeling.ngram import NGram, AddOneNGram
 from languagemodeling.corpus_reader import MyCorpus
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    sents = MyCorpus('../../corpus', 'pibe.txt')
+    sents = MyCorpus('../../corpus', 'Harry.txt')
 
     # train the model
     n = int(opts['-n'])
-    model = NGram(n, sents.sents)
+    usermodel = str(opts['-m'])
+    if usermodel == None or usermodel == 'ngram':
+        model = NGram(n, sents.sents)
+    elif usermodel == 'addone':
+        model = AddOneNGram(n, sents.sents)
+
 
     # save it
     filename = opts['-o']

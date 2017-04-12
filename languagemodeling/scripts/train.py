224308b3225a5aab4e14d8650e9442b2295ad04e
juan_sca <juansca1229@gmail.com>
2017-04-12 12:03:19 -0300

    Models after train with n = 1,2,3,4

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    sents = MyCorpus('../../corpus', 'Harry.txt')
+    sents = MyCorpus('../../corpus', 'big.txt')
 
     # train the model
     n = int(opts['-n'])

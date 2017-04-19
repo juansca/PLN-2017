323b9ec19a9531573fa2c220af85f67574eccd92
juan_sca <juansca1229@gmail.com>
2017-04-19 00:20:05 -0300

    train.py modified to use the models interpolated and interpolatedaddone

--- languagemodeling/scripts/train.py
+++ languagemodeling/scripts/train.py
@@ -1,7 +1,7 @@
 """Train an n-gram model.
 
 Usage:
-  train.py -n <n> [-m <model>] -o <file>
+  train.py -n <n> [-m <model> [-g <gamma>]] -o <file>
   train.py -h | --help
 
 Options:
@@ -9,32 +9,58 @@ Options:
   -m <model>    Model to use [default: ngram]:
                   ngram: Unsmoothed n-grams.
                   addone: N-grams with add-one smoothing.
+                  interpolated: An interpolation model. Without addone smoothing.
+                  interpolatedaddone: An interpolation model with addone smoothing.
+
+  -g <value>    Value of gamma (Only if interpolated is chosen)
+  -a <bool>     Use Addone model for the n = 1 (Only if interpolated is chosen)
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
 from docopt import docopt
 import pickle
 
-from languagemodeling.ngram import NGram, AddOneNGram
+from languagemodeling.ngram import NGram, AddOneNGram, InterpolatedNGram
 from languagemodeling.corpus_reader import MyCorpus
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
 
     # load the data
-    sents = MyCorpus('../../corpus', 'Harry.txt')
+    sents = MyCorpus('../../corpus', 'toTrain.txt')
 
     # train the model
     n = int(opts['-n'])
     usermodel = str(opts['-m'])
 
-    if usermodel == None or usermodel == 'ngram':
+    if usermodel is None or usermodel == 'ngram':
         print("OK! NGram Model is training.")
         model = NGram(n, sents.sents)
     elif usermodel == 'addone':
         print("OK! AddOne Model is training.")
         model = AddOneNGram(n, sents.sents)
-
+    elif usermodel == 'interpolated':
+        gamma = opts['-g']
+        if gamma is None:
+            print("OK! Interpolation Model")
+            print("With a Gamma estimation and addone = False is training.")
+            model = InterpolatedNGram(n, sents.sents, addone=False)
+        else:
+            gamma = float(gamma)
+            print("OK! Interpolation Model")
+            print("With Gamma = ", gamma, " and addone = False is training.")
+            model = InterpolatedNGram(n, sents.sents, gamma=gamma, addone=False)
+    elif usermodel == 'interpolatedaddone':
+        gamma = opts['-g']
+        if gamma is None:
+            print("OK! Interpolation Model with a ")
+            print("Gamma estimation and addone = True is training.")
+            model = InterpolatedNGram(n, sents.sents)
+        else:
+            gamma = float(gamma)
+            print("OK! Interpolation Model with")
+            print("Gamma =", gamma, "and addone = True is training.")
+            model = InterpolatedNGram(1, sents.sents, gamma=gamma)
 
     # save it
     filename = opts['-o']

11006a8b752a257cd81641b0e5022211bbedde81
Franco M. Luque <francolq@famaf.unc.edu.ar>
2017-04-27 12:55:30 -0300

    Change to Ancora 3.0 version.

--- tagging/scripts/train.py
+++ tagging/scripts/train.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
     # load the data
     files = 'CESS-CAST-(A|AA|P)/.*\.tbf\.xml'
-    corpus = SimpleAncoraCorpusReader('ancora/ancora-2.0/', files)
+    corpus = SimpleAncoraCorpusReader('ancora/ancora-3.0.1es/', files)
     sents = list(corpus.tagged_sents())
 
     # train the model

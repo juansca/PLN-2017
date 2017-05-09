d3dfcf5bbbb1834e30443edd12bd46f3fcd5d1de
juan_sca <juansca1229@gmail.com>
2017-05-08 21:39:45 -0300

    Most ten frequent tags information implemented

--- tagging/scripts/stats.py
+++ tagging/scripts/stats.py
@@ -41,3 +41,25 @@ if __name__ == '__main__':
 
     words_vocab_len = len(count_words.keys())
     tags_vocab_len = len(count_tags.keys())
+
+    # Extracting the ten most frequent tags
+    tags_count_list = count_tags.items()
+    tags_count_list = sorted(tags_count_list, key=lambda x: x[1], reverse=True)
+    total_tags = sum([tag_tuple[1] for tag_tuple in tags_count_list])
+
+    ten_frequent_tags = tags_count_list[:10]
+    # Print the table with the information about the tags
+    for i in range(10):
+        tag_tuple = ten_frequent_tags[i]
+        percent = (tag_tuple[1] / total_tags) * 100
+        tag = tag_tuple[0]
+
+        words_with_tag = tag_to_words[tag]
+        words_with_tag = sorted(words_with_tag,
+                                key=lambda word: count_words[word],
+                                reverse=True)
+        five_words = words_with_tag[:5]
+
+        print(i, "th -->", tag,
+              "\tFrequency:", tag_tuple[1], "Percent:", percent,
+              "\tWords most frequent:", five_words)

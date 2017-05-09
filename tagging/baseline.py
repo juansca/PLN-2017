02953765a2d773d556910928f25526a296613c5e
juan_sca <juansca1229@gmail.com>
2017-05-09 15:01:31 -0300

    Implement unknown

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,30 +1,35 @@
+from collections import defaultdict
 
 
 class BaselineTagger:
 
     def __init__(self, tagged_sents):
         """
-        tagged_sents -- training sentences, each one being a list of pairs.
+        :param tagged_sents:training sentences, each one being a list of pairs.
+        :type tagged_sents: list of list of tuples (word, tag)
         """
         pass
 
     def tag(self, sent):
         """Tag a sentence.
 
-        sent -- the sentence.
+        :param sent: the sentence.
+        :type sent: list of words
         """
         return [self.tag_word(w) for w in sent]
 
     def tag_word(self, w):
         """Tag a word.
 
-        w -- the word.
+        :param w: the word.
+        :type w: str
         """
-        return 'nc'
-
+        pass
+        
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
-        w -- the word.
+        :param w: the word.
+        :type w: str
         """
-        return True
+        return w not in self.word_tags

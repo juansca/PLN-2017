acdb8c8d50bf49bf208dcb705b89bf05450b384f
juan_sca <juansca1229@gmail.com>
2017-05-17 00:32:26 -0300

    Optimize eval method using class Counter from collections

--- tagging/scripts/eval.py
+++ tagging/scripts/eval.py
@@ -11,6 +11,7 @@ Options:
 from docopt import docopt
 import pickle
 import sys
+from collections import Counter
 
 from corpus.ancora import SimpleAncoraCorpusReader
 
@@ -39,7 +40,13 @@ if __name__ == '__main__':
 
     # tag
     hits, total = 0, 0
+    hits_known, hits_unknown = 0, 0
+    total_known, total_unknown = 0, 0
+
     n = len(sents)
+
+    are_known = []
+
     for i, sent in enumerate(sents):
         word_sent, gold_tag_sent = zip(*sent)
 
@@ -52,29 +59,40 @@ if __name__ == '__main__':
         total += len(sent)
         total_acc = float(hits) / total
 
-        # Known and unknown score
-        hits_known, hits_uknown = 0, 0
-        total_known, total_unknown = 0, 0
-        for m, g in zip(model_tag_sent, gold_tag_sent):
-            # Unknown word
-            if g == 'nc0s000':
-                # Hit
-                if m == g:
-                    hits_uknown += 1
-                total_unknown += 1
-            # Known word
+        # known words score
+        for j in range(len(hits_sent)):
+            # using the Counter method, descripted later, we have to asign
+            # some values if are known or unknown and if are hit or not.
+            if not model.unknown(word_sent[j]):
+                are_known += [hits_sent[j] + 1]
             else:
-                # Hit
-                if m == g:
-                    hits_known += 1
-                total_known += 1
+                are_known += [hits_sent[j] - 2]
 
         progress('{:3.1f}% (Total: {:2.2f}%)'.format(float(i) * 100 / n,
                                                      total_acc * 100))
+
+    # For eficiency we will use the Counter object from collections
+    # library.
+    # We redefine some things to look for them later
+    known = 2
+    fail_known = 1
+    unknown = -1
+    fail_unknown = -2
+
+    # Counter creates a dictionary whose keys are known, fail_known, unknown
+    # and fail_unknown.
+    counter = Counter(are_known)
+    # Now get the values that represent how many times does apears each one
+    hits_known += counter[known]
+    total_known += counter[known] + counter[fail_known]
+
+    hits_unknown += counter[unknown]
+    total_unknown += counter[unknown] + counter[fail_unknown]
+
     # Compute accuracy
     total_acc = float(hits) / total
     known_acc = float(hits_known) / total_known
-    unknown_acc = float(hits_uknown) / total_unknown
+    unknown_acc = float(hits_unknown) / total_unknown
 
     print('')
     print('Total accuracy: {:2.2f}%'.format(total_acc * 100))

d8f009b6313e548c8d1e95a095d42e4bc7b0af20
juan_sca <juansca1229@gmail.com>
2017-05-17 17:59:50 -0300

    Modify train.py to train mmem model with clasifier options

--- tagging/scripts/train.py
+++ tagging/scripts/train.py
@@ -1,7 +1,7 @@
 """Train a sequence tagger.
 
 Usage:
-  train.py [-m <model> [-n <order>]] -o <file>
+  train.py [-m <model> [-n <order> -c <clasifier>]] -o <file>
   train.py -h | --help
 
 Options:
@@ -11,6 +11,8 @@ Options:
                   hmm: Hidden Markov Model
                   mlhmm: Maximum Likelihood Hidden Markov Model
                   memm: Maximum Entropy Markov Models
+  -c <clasifier> Clasifier that use in Maximum Entropy Markov Model
+                 [default: LogisticRegression]
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
@@ -18,6 +20,9 @@ from docopt import docopt
 import pickle
 
 from corpus.ancora import SimpleAncoraCorpusReader
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+
 from tagging.baseline import BaselineTagger
 from tagging.hmm import HMM, MLHMM
 from tagging.mem import MEMM
@@ -29,6 +34,10 @@ models = {
     'memm': MEMM,
 }
 
+clasifiers = {
+    'multinomial': MultinomialNB,
+    'linear': LinearSVC
+}
 
 if __name__ == '__main__':
     opts = docopt(__doc__)
@@ -41,9 +50,14 @@ if __name__ == '__main__':
     # train the model
     n = int(opts['-n'])
     m = opts['-m']
-    if m == 'mlhmm' or m == 'memm':
+    c = opts['-c']
+
+    if m == 'mlhmm':
         print("Model", m, "Training with n =", n)
         model = models[m](n, sents)
+    elif m == 'memm':
+        clasifier = clasifiers[c]
+        print("Model", m, "Training with clasifier", c, "and n =", n,)
     else:
         print("Model", m, "Training")
         model = models[m](sents)

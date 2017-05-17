a6b5aea4da805f708be8bfbefcf8ff4eeb4c6a76
juan_sca <juansca1229@gmail.com>
2017-05-17 00:33:07 -0300

    Add MLHMM option to train the model

--- tagging/scripts/train.py
+++ tagging/scripts/train.py
@@ -1,12 +1,15 @@
 """Train a sequence tagger.
 
 Usage:
-  train.py [-m <model>] -o <file>
+  train.py [-m <model> [-n <order>]] -o <file>
   train.py -h | --help
 
 Options:
+  -n <order>    Order of the model trained
   -m <model>    Model to use [default: base]:
                   base: Baseline
+                  hmm:
+                  mlhmm:
   -o <file>     Output model file.
   -h --help     Show this screen.
 """
@@ -15,10 +18,13 @@ import pickle
 
 from corpus.ancora import SimpleAncoraCorpusReader
 from tagging.baseline import BaselineTagger
+from tagging.hmm import HMM, MLHMM
 
 
 models = {
     'base': BaselineTagger,
+    'hmm': HMM,
+    'mlhmm': MLHMM,
 }
 
 
@@ -31,7 +37,14 @@ if __name__ == '__main__':
     sents = list(corpus.tagged_sents())
 
     # train the model
-    model = models[opts['-m']](sents)
+    n = int(opts['-n'])
+    m = opts['-m']
+    if m == 'mlhmm':
+        print("Model", m, "Training with n =", n)
+        model = models[m](n, sents)
+    else:
+        print("Model", m, "Training")
+        model = models[m](sents)
 
     # save it
     filename = opts['-o']

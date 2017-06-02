fe354c86f3265aabdc3c6db43032ba77c4e31715
juan_sca <juansca1229@gmail.com>
2017-06-02 18:09:04 -0300

    Add to_triple and binary_productions private methods. Implement to_triple

--- parsing/cky_parser.py
+++ parsing/cky_parser.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from nltk.tree import Tree
+
+
 class CKYParser:
 
     def __init__(self, grammar):
@@ -6,9 +10,33 @@ class CKYParser:
         """
         pass
 
+    def _to_triple(self, prod):
+        """Convert the production prod to a triple.
+
+        :param prod: production
+
+        :return 3-uple (left_hand_side, right_hand_side, prob)
+        """
+        left_hand = str(prod.lhs())
+        right_hand = tuple(str(rhs) for rhs in prod.rhs())
+        prob = prod.logprob()
+
+        return (left_hand, right_hand, prob)
+
+    def _binary_productions(self, Bs, Cs):
+        """Create a list of tuples containing all rules with the form A -> B C
+        and its probability. Where B is in Bs and C is in Cs.
+
+        :param Bs: list of nonterminals or terminals
+        :param Cs: list of nonterminals or terminals
+
+        :return: 4-uple (A, B, C, prob)
+        """
+        pass
+
     def parse(self, sent):
         """Parse a sequence of terminals.
 
         :param sent: the sequence of terminals.
         """
-        pass
+        pass 

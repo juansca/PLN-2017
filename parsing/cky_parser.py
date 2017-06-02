0512da0ae662bb89d039de3dd7ddb9f7ff7103f7
juan_sca <juansca1229@gmail.com>
2017-06-02 14:29:08 -0300

    First commit ex2

---
+++ parsing/cky_parser.py
@@ -0,0 +1,14 @@
+class CKYParser:
+
+    def __init__(self, grammar):
+        """
+        :param grammar: a binarised NLTK PCFG.
+        """
+        pass
+
+    def parse(self, sent):
+        """Parse a sequence of terminals.
+
+        :param sent: the sequence of terminals.
+        """
+        pass
